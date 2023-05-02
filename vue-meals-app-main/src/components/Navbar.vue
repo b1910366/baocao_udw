ae157465bdb9665fdda85db450d1d390ec480e2b
b1910366 <duocb1910366@student.ctu.edu.vn>
2023-05-02 15:41:47 +0700

    Upload code to Github

---
+++ vue-meals-app-main/src/components/Navbar.vue
@@ -0,0 +1,30 @@
+<template>
+  <header class="bg-white shadow h-16 flex justify-between items-stretch">
+    <router-link
+      :to="{ name: 'home' }"
+      class="inline-flex items-center h-full px-5 font-bold"
+    >
+      Ứng dụng Web
+    </router-link>
+    <div class="flex items-center gap-1">
+      <router-link
+        :to="{ name: 'byName' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-gray-100 border rounded hover:scale-105"
+      >
+        Search Meals
+      </router-link>
+      <router-link
+        :to="{ name: 'byLetter' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-gray-100 border rounded hover:scale-105"
+      >
+        Meals By Letter
+      </router-link>
+      <router-link
+        :to="{ name: 'ingredients' }"
+        class="inline-flex items-center px-2 h-full transition-colors hover:bg-gray-100 border rounded hover:scale-105"
+      >
+        Meals By Ingredients
+      </router-link>
+    </div>
+  </header>
+</template>
